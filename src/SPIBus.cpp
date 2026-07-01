423e27b2737f56e3e18e2824ca7b7d81a409e491
Pooria Nekuie <31559510+Pooria-nek@users.noreply.github.com>
2026-07-01 12:04:44 +0330

    Create SPIBus.cpp

---
+++ src/SPIBus.cpp
@@ -0,0 +1,81 @@
+#include "SPIBus.h"
+
+namespace MemoryCore
+{
+
+SPIBus::SPIBus(
+    SPIClass& spi,
+    uint8_t chipSelectPin)
+    : _spi(spi),
+      _csPin(chipSelectPin),
+      _settings(
+          MEMORYCORE_SPI_CLOCK,
+          MEMORYCORE_SPI_BIT_ORDER,
+          MEMORYCORE_SPI_MODE)
+{
+}
+
+void SPIBus::begin()
+{
+    pinMode(_csPin, OUTPUT);
+    digitalWrite(_csPin, HIGH);
+
+    _spi.begin();
+}
+
+void SPIBus::beginTransaction()
+{
+    _spi.beginTransaction(_settings);
+    digitalWrite(_csPin, LOW);
+}
+
+void SPIBus::endTransaction()
+{
+    digitalWrite(_csPin, HIGH);
+    _spi.endTransaction();
+}
+
+uint8_t SPIBus::transfer(uint8_t data)
+{
+    return _spi.transfer(data);
+}
+
+void SPIBus::transfer(
+    const uint8_t* txBuffer,
+    uint8_t* rxBuffer,
+    size_t length)
+{
+    for (size_t i = 0; i < length; i++)
+    {
+        uint8_t tx = txBuffer ? txBuffer[i] : 0xFF;
+        uint8_t rx = _spi.transfer(tx);
+
+        if (rxBuffer)
+        {
+            rxBuffer[i] = rx;
+        }
+    }
+}
+
+void SPIBus::write(
+    const uint8_t* data,
+    size_t length)
+{
+    while (length--)
+    {
+        _spi.transfer(*data++);
+    }
+}
+
+void SPIBus::read(
+    uint8_t* data,
+    size_t length,
+    uint8_t filler)
+{
+    while (length--)
+    {
+        *data++ = _spi.transfer(filler);
+    }
+}
+
+} // namespace MemoryCore
